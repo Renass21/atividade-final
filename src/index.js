96d8eb59b3c60f7bde76e57a04fce3d4d0b85149
Renass21 <renatodossantoss21@gmail.com>
2023-08-02 22:54:23 -0300

    installCors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/", function (requisicao, resposta) {
   resposta.status(200);
